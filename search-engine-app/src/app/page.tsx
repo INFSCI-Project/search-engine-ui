569e4a5e3e76bb97d906b55e27690e38f84f5256
akshatsabavat <akshat.sabavat@gmail.com>
2024-12-12 03:59:28 +0530

    Chore: installed chakra ui

--- search-engine-app/src/app/page.tsx
+++ search-engine-app/src/app/page.tsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Hi there</div>;
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col w-[600px]">
+        <div>hi</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

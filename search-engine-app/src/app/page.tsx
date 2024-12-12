6d4f79d399a7854bddcf5047f1e8ac1715d78e4d
akshatsabavat <akshat.sabavat@gmail.com>
2024-12-12 10:15:11 +0530

    Created: Search Bar component

--- search-engine-app/src/app/page.tsx
+++ search-engine-app/src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import SearchBar from "@/components/search/SearchBar";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ArrowRight } from "lucide-react";
@@ -6,24 +7,8 @@ import { ArrowRight } from "lucide-react";
 const Home = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col">
-        {/* Search Bar */}
-        <div className="relative">
-          <Input
-            type="search"
-            placeholder="Search..."
-            className="pr-10"
-            onChange={(e) => console.log(e.target.value)}
-          />
-          <Button
-            type="submit"
-            size="sm"
-            className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 p-0 bg-gradient-to-r from-purple-500 to-purple-700 hover:from-purple-600 hover:to-purple-800 text-white"
-          >
-            <ArrowRight className="h-4 w-4" />
-            <span className="sr-only">Search</span>
-          </Button>
-        </div>
+      <div className="flex flex-col w-[550px]">
+        <SearchBar />
       </div>
     </div>
   );
---
+++ search-engine-app/src/components/search/SearchBar.tsx
@@ -0,0 +1,26 @@
+import { ArrowRight } from "lucide-react";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+const SearchBar = () => {
+  return (
+    <div className="relative w-full">
+      <Input
+        type="search"
+        placeholder="Any thing you wanna know..."
+        className="pr-10 bg-white h-10"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Button
+        type="submit"
+        size="sm"
+        className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 p-0 bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white transition-all duration-300 ease-in-out"
+      >
+        <ArrowRight className="h-4 w-4" />
+        <span className="sr-only">Search</span>
+      </Button>
+    </div>
+  );
+};
+
+export default SearchBar;

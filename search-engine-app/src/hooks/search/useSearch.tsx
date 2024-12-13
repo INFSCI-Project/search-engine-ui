f6de778d5bbca8cee3c46c50204acb8d71e290ab
akshatsabavat <akshat.sabavat@gmail.com>
2024-12-14 03:58:39 +0530

    Created: functionality to mark performance time

--- search-engine-app/src/hooks/search/useSearch.tsx
+++ search-engine-app/src/hooks/search/useSearch.tsx
@@ -5,20 +5,37 @@ import { useAtom } from "jotai";
 import { useState } from "react";
 
 const useSearch = () => {
+  const [queryRetrievalTime, setQueryRetrievalTime] = useState<string | null>(
+    null
+  );
   const [isLoadingSeachResults, setisLoadingSeachResults] =
     useState<boolean>(false);
   const [{ queryResults }, setQueryResults] = useAtom(queryResultsAtom);
   const handleSearchQuery = async (search_query: string) => {
     try {
       setisLoadingSeachResults(true);
+      const start_time = performance.now();
+
+      // fetching response from server
       const response = await axios.post("http://127.0.0.1:3000/search", {
         query: `${search_query}`,
       });
+
+      // calculating query search time
+      const end_time = performance.now();
+      const duration_in_seconds = (end_time - start_time) / 1000;
+      const duration_in_minutes = (duration_in_seconds / 60).toFixed(2);
+      setQueryRetrievalTime(`${duration_in_minutes} minutes`);
+
+      // transforming and typecasing response data from the server
       const data = response.data as SearchQueryResponseType;
       setQueryResults({ queryResults: data });
       setisLoadingSeachResults(false);
+
+      // redirection to the results page
       window.location.href = "/results";
     } catch (err) {
+      // alter for errors during search results
       setisLoadingSeachResults(false);
       console.log(err);
     }
